4122cadde41c7f89bbbec688637fe7d7f5c62597
Muhammad Usman <121295349+MuhammadUsmanAnsari@users.noreply.github.com>
2023-11-26 22:35:15 +0500

    task_completed

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { registerUser, loginUser, updateUser } = require('../controller/userController')
-const { protect } = require('../middleware/authMIddleWare')
-const { upload } = require('../utils/upload')
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
-router.put('/update', upload.single("image"), updateUser)
+router.put('/update', updateUser)
 
 module.exports = router
